46670a511a716963d9ab3ccfa0cd716d44864740
ambrishyad85 <ambrishyad85@gmail.com>
2023-12-13 19:41:26 +0530

    change for load the page or stories

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-import { InsertStoriesComponent } from './insert-stories/insert-stories.component';
 import { ViewStoriesComponent } from './view-stories/view-stories.component';
-import { InsertALLRecordsCahesComponent } from './insert-allrecords-cahes/insert-allrecords-cahes.component';
 
 
 
-const routes: Routes = [ 
-  { path: "Home", component: AppComponent }, 
-  { path: "ViewStories", component: ViewStoriesComponent }, 
-  
-  
+const routes: Routes = [  
+  { path: "ViewStories",component: ViewStoriesComponent }
 ]; 
 
 @NgModule({
--- src/app/search-recods-pipe.pipe.ts
+++ src/app/search-recods-pipe.pipe.ts
@@ -4,29 +4,38 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'searchRecodsPipe'
 })
 export class SearchRecodsPipePipe implements PipeTransform {
-
   transform(list: any[], value: string) {
-  
-    //list.filter(item => item.id === value)
+   
+   list= list.filter(x=>x.url!=null);
     if(value!=null && value!='')
     {
-      if(list.filter(item => item.id==value).length!=0)
-      {
-        return list.filter(item => item.id==value);  
-      }
-      else if(list.filter(item => item.title==value).length!=0)
-      {
-        return list.filter(item => item.title==value);  
-      }
-      else if(list.filter(item => item.url==value).length!=0)
-      {
-        return list.filter(item => item.url==value);
-      }
+      debugger
+      if(value === undefined || value === null) return list;
       
+      if(list.filter(function(element) {
+                return element.title.toLowerCase().includes(value.toLocaleLowerCase());
+      }).length>0)
+      {
+        return list.filter(function(element) {
+          return element.title.toLowerCase().includes(value.toLocaleLowerCase());
+          })
+      }
+      else if(list.filter(function(element) {
+        return element.url.toLowerCase().includes(value.toLocaleLowerCase());
+        }).length>0)
+            {
+                return list.filter(function(element) {
+                  return element.url.toLowerCase().includes(value.toLocaleLowerCase());
+              })
+    }
+    else if(list.filter(x=>x.id==value).length>0)
+          {
+              return list.filter(x=>x.id==value);
+          }
+
     }
 
     return list;
-    //return value ? list.filter(item => item.id==value): list;  
-
+   
   }
 }
